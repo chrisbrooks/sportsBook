9c2f66efbe635a27d01e471233e20008842766b2
chrisbrooks <chris.brooks@orange.com>
2014-11-13 16:20:00 +0000

    adding new app

---
+++ GruntFile.js
@@ -0,0 +1,270 @@
+var scriptsList = {
+	'app/build/scripts/combined.min.js': [
+		'app/src/assets/scripts/vendor/jquery-1.11.0.min.js',
+		'app/src/assets/scripts/vendor/jquery.easing.1.3.js',
+		'app/src/assets/scripts/vendor/handlebars-v1.3.0.js',
+		'app/src/assets/scripts/custom/main.js',
+	]
+};
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		jshint: {
+
+			files: 'app/src/assets/scripts/custom/*.js',
+
+			options: {
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true
+				}
+			}
+		},
+
+		uglify: {
+
+			development : {
+				options: {
+					beautify: true,
+					compress: false
+				},
+				files: scriptsList
+			},
+
+			release : {
+				files: scriptsList
+			}
+		},
+
+		sass: {
+
+			development: {
+				options: {
+				},
+				files: {
+					'app/build/styles/app.css': 'app/src/assets/styles/app.scss',
+				}
+			},
+
+			release: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'app/build/styles/app.css': 'app/src/assets/styles/app.scss',
+				}
+			}
+		},
+
+		autoprefixer: {
+
+			build: {
+				expand: true,
+				flatten: true,
+				src: 'app/build/styles/**/*.css',
+				dest: 'app/build/styles/'
+			}
+		},
+
+		copy: {
+
+			pages: {
+				files: [{
+					expand: true,
+					cwd: 'app/src/pages/',
+					src: ['**/*.html'],
+					dest: 'app/build/'
+				}]
+			},
+
+			images: {
+				files: [{
+					expand: true,
+					cwd: 'app/src/assets/images',
+					src: ['**/*.{png,jpg,gif,svg}'],
+					dest: 'app/build/images/'
+				}]
+			},
+
+			media: {
+				files: [{
+					expand: true,
+					cwd: 'app/src/assets/media',
+					src: ['**/*.{png,jpg,gif,svg}'],
+					dest: 'app/build/media/'
+				}]
+			},
+
+			fonts: {
+				files: [
+					{
+						expand: true,
+						cwd: 'app/src/assets/fonts',
+						src: ['**/*.{eot,svg,ttf,woff}'],
+						dest: 'app/build/fonts/'
+					}
+				]
+			},
+
+			videos: {
+				files: [{
+					expand: true,
+					cwd: 'app/src/assets/video',
+					src: ['**/*'],
+					dest: 'app/build/video/'
+				}]
+			},
+
+			pdf: {
+				files: [{
+					expand: true,
+					cwd: 'app/src/assets',
+					src: ['**/*.pdf'],
+					dest: 'app/build/'
+				}]
+			}
+		},
+
+		clean: {
+
+			build: {
+				src: ['app/build/']
+			}
+		},
+
+		connect: {
+
+			server: {
+				options: {
+					base: 'app/build/',
+					hostname: '*',
+					keepalive: false,
+					livereload: false,
+					open: {
+						target: 'http://localhost:8000'
+					},
+					port: 8000,
+					middleware: function(connect, options, middlewares) {
+
+						middlewares.unshift(function (req, res, next) {
+
+							var fs = require('fs');
+							var path = require('path');
+							var support = ['POST', 'PUT', 'DELETE'];
+
+							if (support.indexOf(req.method.toUpperCase()) != -1) {
+								var filepath = path.join(options.base[0], req.url);
+								if (fs.existsSync(filepath) && fs.statSync(filepath).isFile()) {
+									return res.end(fs.readFileSync(filepath));
+								}
+							}
+
+							return next();
+						});
+
+						return middlewares;
+					}
+				}
+			}
+		},
+
+		watch: {
+
+			// Watch for markup changes
+			markup: {
+				files: 'app/src/pages/*.html',
+				tasks: ['copy:pages'],
+				options: {
+					livereload: false,
+					interrupt: true
+				}
+			},
+
+			// Run SASS compliler when precompiled files are changed
+			styles: {
+				files: 'app/src/assets/styles/**/*.scss',
+				tasks: ['sass:development'],
+				options: {
+					livereload: false,
+					spawn: false
+				}
+			},
+
+			images: {
+				files: ['app/src/assets/images/**/*.{png,jpg,gif}'],
+				tasks: ['copy:images'],
+				options: {
+					livereload: false,
+					interrupt: true
+				}
+			},
+
+			media: {
+				files: ['app/src/assets/media/**/*.{png,jpg,gif}'],
+				tasks: ['copy:media'],
+				options: {
+					livereload: false,
+					interrupt: true
+				}
+			},
+
+			videos: {
+				files: ['app/src/assets/video/**/*.{mp4,ogv,webm}'],
+				tasks: ['copy:videos'],
+				options: {
+					livereload: false,
+					interrupt: true
+				}
+			},
+
+			scripts: {
+				files: ['app/src/assets/scripts/**/*.js'],
+				tasks: ['uglify:development'],
+				options: {
+					livereload: false,
+					interrupt: true
+				}
+			},
+
+		},
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-html-validation');
+
+	// Register task(s) to run when 'grunt' command is executed
+	grunt.registerTask('default', ['development']);
+
+	// Register task(s) to run when 'grunt development' command is executed
+	grunt.registerTask('development', [
+		'jshint',
+		'clean',
+		'sass:development',
+		'autoprefixer',
+		'copy',
+		'uglify:development',
+		'connect',
+		'watch'
+	]);
+
+	// Register task(s) to run when 'grunt release' command is executed
+	grunt.registerTask('release', [
+		'clean',
+		'sass:release',
+		'autoprefixer',
+		'copy',
+		'uglify:release',
+	]);
+
+};
---
+++ app/src/assets/scripts/custom/main.js
@@ -0,0 +1,152 @@
+(function (window, $, undefined) {
+
+	"use strict";
+
+	var methods		= {};
+
+	methods.init = function () {
+
+		function datastorage(searchurl) {
+			var storedData = JSON.parse(localStorage.getItem(searchurl)),
+				currentTime,
+				storedTime,
+				difference,
+				numminutes;
+
+			if (storedData) { // if storedData is not null or undefined, it exists, so we can check time difference without causing an error
+				currentTime = new Date();
+				storedTime = new Date(storedData.storedTime); // this line was breaking it because storedData.storedTime didn't exist on first visit or after clearing localStorage
+				difference = currentTime - storedTime;
+				numminutes = Math.round(((difference % 86400000) % 3600000) / 60000);
+			}
+
+			if (storedData && numminutes < 10) {
+				goDataGo(storedData);
+			} else {
+				ajaxcall(searchurl);
+			}
+		}
+
+		function ajaxcall(searchurl) {
+			$.ajax({
+				type: "POST",
+				url: 'http://content.guardianapis.com/search?api-key=5vk4jfy7myw4rdgfhg2hx7tn&show-fields=all&order-by=newest&q='+ searchurl +'&ion=sport',
+				dataType: 'JSONP',
+				success: function(data) {
+					data.storedTime = new Date();
+
+					localStorage.setItem(searchurl, JSON.stringify(data));
+
+					if ($('ul.tabs li').hasClass('current')) {
+						goDataGo(data);
+					}
+				},
+				beforeSend: function() {
+					$('body').append($("<div class='loading'/>"));
+					$('.loading').fadeIn();
+				},
+				complete: function() {
+					$('.loading').remove();
+				}
+			}).fail(function() {
+				console.log('ajax call failed');
+			});
+		}
+
+		function goDataGo(data) {
+			var results = data.response.results;
+
+			for (var i = 0; i < results.length; i += 1) {
+				DateTime(data, i, results);
+				headlineSuccess(results[i]);
+			}
+
+			fullStory(data);
+		}
+
+		function DateTime(data, i, results){
+
+			var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+			var timeDate = results[i].webPublicationDate,
+					date = new Date(timeDate),
+					newDate = date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
+
+				results[i].newDate = newDate;
+
+				var Datetime = results[i].webPublicationDate,
+					time = new Date(Datetime),
+					newTime = ('0' + (time.getHours())).slice(-2) + "." + ('0' + (time.getMinutes())).slice(-2);
+
+				results[i].newTime = newTime;
+		}
+
+		function fullStory(data){
+			$('.story').on('click', function() {
+				$(window).scrollTop('0px');
+				var index = $(this).index(),
+					results = data.response.results[index],
+					currentStory = results.fields.body,
+					currentTrailText = results.fields.trailText,
+					currentHeadline = results.webTitle;
+
+				if (undefined !== currentStory && currentStory.length > 100) {
+					$('.full-story').append("<div class='generalpic'></div><h1 class='currentHeadline'>" + currentHeadline + "</h1><div class='currentTrailText'>" + currentTrailText + "</div><div class='currentStory'>" + currentStory + "</div>").addClass('opened');
+					$('.content-wrapper, .menu, .back').addClass('opened');
+
+				} else {
+					var storyLink = $(this).find("a");
+					storyLink.attr("target", "_blank");
+					window.open(storyLink.attr("href"));
+				}
+
+			});
+		}
+
+		function headlineSuccess(data) {
+			var source = $("#headlines-success").html(),
+				template = Handlebars.compile(source),
+				html = template(data);
+			$('.tab-content.current .news-story').append(html);
+		}
+
+		function menuClick(item){
+			var tab_id = item.attr('data-tab');
+			$('ul.tabs li').removeClass('current');
+			$('.tab-content').removeClass('current');
+			item.addClass('current');
+			$("#" + tab_id).addClass('current');
+
+			var searchRes = item.attr('data-role');
+
+			if ($("#" + tab_id).find('.story').length < 1 && item.hasClass('current')) {
+				datastorage(searchRes);
+			}
+
+			$('.header p.sportstitle').empty();
+			$('.header p.sportstitle').append(item.find('p').html() + ' News');
+		}
+
+		$(document).ready(function() {
+			datastorage('sport');
+
+			$('.menu, ul li').on('click', function() {
+				$('ul, .menu, .content-wrapper, .full-story').toggleClass('active');
+			});
+
+			$('.back').on('click', function() {
+				$('.content-wrapper, .full-story, .menu, .back').toggleClass('opened');
+				setTimeout(function(){
+					$('.full-story').empty();
+				}, 500);
+			});
+
+			$('ul.tabs li').on('click', function() {
+				var item = $(this);
+				menuClick(item);
+			});
+		});
+	};
+
+	methods.init();
+
+}(window, jQuery));
